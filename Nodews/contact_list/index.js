4b3a16038d59a4d399d51e29b5543f62292d49a9
Zahid Khan <zahidkhanavi@gmail.com>
2024-07-31 20:53:47 +0530

    created a contact list

--- Nodews/contact_list/index.js
+++ Nodews/contact_list/index.js
@@ -1,12 +1,42 @@
+const { name } = require('ejs');
 const express = require('express');
+const path = require('path');
+const { title } = require('process');
 const port = 8000;
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 
+var contactlist = [
+    {name:"Arpan",
+     phone: "111111111111"
+    },
+    {
+        name: "Tony Stark",
+        phone: "123456789"
+    }, 
+    {
+        name: "Coding Ninjas",
+        phone: "2316537634"
+    }
+]
 app.get('/', function(req, res){
-    console.log(req);
-    res.send('<h1>Cool, it is running! or is it?</h1>');
+    // console.log(__dirname);
+    // res.send('<h1>Cool, it is running! or is it?</h1>');
+    return res.render('home', {title: "Contact List",
+        contact_list: contactlist
+    });
+});
+
+app.get('/practice', function(req, res){
+    return res.render('practice', {
+        title: "Let us play with ejs"
+    });
+}); 
+app.post('/create-contact', function(req, res){
+    return res.redirect('/practice');
 });
 
 app.listen(port, function(err){
