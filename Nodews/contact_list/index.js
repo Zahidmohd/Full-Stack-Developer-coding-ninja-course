63cd87098d117197b7bf8f716910547e39da3616
Zahid Khan <zahidkhanavi@gmail.com>
2024-08-03 14:12:03 +0530

    added assets to contact list

--- Nodews/contact_list/index.js
+++ Nodews/contact_list/index.js
@@ -7,15 +7,31 @@ const port = 8000;
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded());
+app.use(express.static('assets'));
 
+// middleware1
+// app.use(function(req, res, next){
+//     req.myName="Zahid"
+//     // console.log('middleware 1 called');
+//     next();
+// });
+
+// middleware2
+// app.use(function(req, res, next){
+//     console.log('My Name from MW2', req.myName);
+//     // console.log('middleware 2 called');
+//     next(); 
+//     });
 
 var contactlist = [
-    {name:"Arpan",
-     phone: "111111111111"
+    {
+        name: "Arpan",
+        phone: "1111111111"
     },
     {
         name: "Tony Stark",
-        phone: "123456789"
+        phone: "0123456789"
     }, 
     {
         name: "Coding Ninjas",
@@ -23,6 +39,7 @@ var contactlist = [
     }
 ]
 app.get('/', function(req, res){
+    // console.log('from the get route controller', req.myName);  
     // console.log(__dirname);
     // res.send('<h1>Cool, it is running! or is it?</h1>');
     return res.render('home', {title: "Contact List",
@@ -36,7 +53,15 @@ app.get('/practice', function(req, res){
     });
 }); 
 app.post('/create-contact', function(req, res){
-    return res.redirect('/practice');
+    contactlist.push(req.body);
+    // contactlist.push({
+    //     name: req.body.name,
+    //     phone: req.body.phone
+    // });
+    //  console.log(req.body);
+    //  console.log(req.body.name);
+    //  console.log(req.body.phone);
+    return res.redirect('back');
 });
 
 app.listen(port, function(err){
